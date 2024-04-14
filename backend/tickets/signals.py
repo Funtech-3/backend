0119fcf34844b47a60c979f7e3104c90006c5029
floks41 <floks41@yandex.com>
2024-04-14 17:56:23 +0300

    fix signal for loading new data 4

--- backend/tickets/signals.py
+++ backend/tickets/signals.py
@@ -8,19 +8,20 @@ from users.models import NotificationSwitch
 @receiver(pre_save, sender=Registration)
 def watch_registration_status(sender, instance, **kwargs):
     print("Get pre_save model Registration signal.")
+    is_notification = False
+    is_email = False
+    old_status = ""
     try:
         old_status = Registration.objects.get(pk=instance.pk).status
-        is_notification = Registration.objects.get(
-            pk=instance.pk
-        ).user.user_notifications.is_notification
-        is_email = Registration.objects.get(
-            pk=instance.pk
-        ).user.user_notifications.is_email
-    except Registration.DoesNotExist:
-        old_status = ""
-    except NotificationSwitch.DoesNotExist:
-        is_notification = False
-        is_email = False
+        if Registration.objects.get(pk=instance.pk):
+            is_notification = Registration.objects.get(
+                pk=instance.pk
+            ).user.user_notifications.is_notification
+            is_email = Registration.objects.get(
+                pk=instance.pk
+            ).user.user_notifications.is_email
+    except (Registration.DoesNotExist, NotificationSwitch.DoesNotExist):
+        pass
     new_status = instance.status
     if (
         old_status != new_status
