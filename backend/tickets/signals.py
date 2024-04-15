d832011ec1f97ad2e194f6f447eec42903df4473
floks41 <floks41@yandex.com>
2024-04-15 10:09:58 +0300

    fix review

--- backend/tickets/signals.py
+++ backend/tickets/signals.py
@@ -10,25 +10,14 @@ def watch_registration_status(sender, instance, **kwargs):
     """Сигнал для модели Registration отслеживает событие перед сохранение.
     Если статус регистрации меняется на CONFIRMED и включены уведомления
     на email, на почту клиента."""
-    is_notification = False
-    is_email = False
     old_status = ""
     try:
         old_status = Registration.objects.get(pk=instance.pk).status
-        if Registration.objects.get(pk=instance.pk):
-            is_notification = Registration.objects.get(
-                pk=instance.pk
-            ).user.user_notifications.is_notification
-            is_email = Registration.objects.get(
-                pk=instance.pk
-            ).user.user_notifications.is_email
-    except (Registration.DoesNotExist, NotificationSwitch.DoesNotExist):
+    except Registration.DoesNotExist:
         pass
     new_status = instance.status
     if (
         old_status != new_status
         and new_status == Registration.Status.CONFIRMED
-        and is_notification
-        and is_email
     ):
         send_ticket_info(instance)
